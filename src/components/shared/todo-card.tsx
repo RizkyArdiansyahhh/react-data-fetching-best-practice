575ae5636341d74807c16e034c7b89f9f3fc26d4
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-10-17 10:10:39 +0700

    feat : delete todo

--- src/components/shared/todo-card.tsx
+++ src/components/shared/todo-card.tsx
@@ -1,26 +1,30 @@
 import { CircleCheckBig, Trash, UndoDot } from "lucide-react";
-import Link from "next/link";
+import { Button } from "../ui/button";
 
 type TodoCardProps = {
+  id: number;
   title: string;
   isCompleted: boolean;
+  deleteTodoFn: (id: number) => void;
+  // undoTodoFn: (id: number) => void;
 };
 
 export default function TodoCard(props: TodoCardProps) {
-  const { title, isCompleted } = props;
+  const { id, title, isCompleted, deleteTodoFn } = props;
   return (
     <div className="bg-white rounded-md w-full p-3">
       <p className="text-xl font-semibold">{title}</p>
       <div className="w-full h-0.5 bg-foreground/10 my-3"></div>
       <div className="flex flex-row gap-3 justify-end">
-        <Link
-          href={""}
+        <Button
+          variant={"secondary"}
           className="hover:scale-110 transition-all ease-in-out duration-200"
         >
           <CircleCheckBig size={"25"} color="green" />
-        </Link>
-        <Link
-          href={""}
+        </Button>
+        <Button
+          onClick={() => !isCompleted && deleteTodoFn(id)}
+          variant={"secondary"}
           className="hover:scale-110 transition-all ease-in-out duration-200"
         >
           {isCompleted ? (
@@ -28,7 +32,7 @@ export default function TodoCard(props: TodoCardProps) {
           ) : (
             <Trash color="red" size={"25"} />
           )}
-        </Link>
+        </Button>
       </div>
     </div>
   );
